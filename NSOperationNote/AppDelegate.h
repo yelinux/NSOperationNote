84b1552b8d808d5fd6255ad9d2a9f7a8f9b867d9
chenyehong <ye_linux@126.com>
2021-04-15 09:43:49 +0800

    Initial Commit

---
+++ NSOperationNote/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSOperationNote
+//
+//  Created by chenyehong on 2021/4/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NSOperationNote/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  NSOperationNote
+//
+//  Created by chenyehong on 2021/4/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
